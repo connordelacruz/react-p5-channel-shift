1a742eb05dcf32565a0b9ae7f4b1eb113ae88ddb
Connor de la Cruz <connor.c.delacruz@gmail.com>
2024-01-21 17:08:37 -0600

    channel swap method

--- src/ChannelShiftSketch.jsx
+++ src/ChannelShiftSketch.jsx
@@ -1,5 +1,9 @@
 
 export function ChannelShiftSketch(p5) {
+  const R_OFFSET = 0
+  const G_OFFSET = 1
+  const B_OFFSET = 2
+  const A_OFFSET = 3
 
   let sourceImage, rImage, gImage, bImage
 
@@ -44,9 +48,33 @@ export function ChannelShiftSketch(p5) {
 
   p5.draw = () => {
     p5.background(0)
+    // TODO: figure out how to wrap image when it extends past the canvas
+    // TODO TESTING
+    let [newRImage, newBImage] = swapChannels(rImage, R_OFFSET, bImage, B_OFFSET)
     // Blend RGB channels
-    p5.blend(rImage, 0, 0, rImage.width, rImage.height, 0, 0, p5.width, p5.height, p5.ADD)
+    p5.blend(newRImage, 0, 0, rImage.width, rImage.height, 0, 0, p5.width, p5.height, p5.ADD)
+    // p5.blend(rImage, 0, 0, rImage.width, rImage.height, 0, 0, p5.width, p5.height, p5.ADD)
     p5.blend(gImage, 0, 0, gImage.width, gImage.height, 0, 0, p5.width, p5.height, p5.ADD)
-    p5.blend(bImage, 0, 0, bImage.width, bImage.height, 0, 0, p5.width, p5.height, p5.ADD)
+    // p5.blend(bImage, 0, 0, bImage.width, bImage.height, 0, 0, p5.width, p5.height, p5.ADD)
+    p5.blend(newBImage, 0, 0, bImage.width, bImage.height, 0, 0, p5.width, p5.height, p5.ADD)
+  }
+
+  // TODO: doc n implement
+  function swapChannels(channelImage0, channelOffset0, channelImage1, channelOffset1) {
+    let newChannelImage0 = p5.createImage(channelImage0.width, channelImage1.height)
+    newChannelImage0.loadPixels()
+    let newChannelImage1 = p5.createImage(channelImage1.width, channelImage1.height)
+    newChannelImage1.loadPixels()
+
+    for (let i = 0; i < newChannelImage0.pixels.length; i += 4) {
+      newChannelImage0.pixels[i + channelOffset0] = channelImage1.pixels[i + channelOffset1]
+      newChannelImage0.pixels[i + A_OFFSET] = 255
+      newChannelImage1.pixels[i + channelOffset1] = channelImage0.pixels[i + channelOffset0]
+      newChannelImage1.pixels[i + A_OFFSET] = 255
+    }
+    newChannelImage0.updatePixels()
+    newChannelImage1.updatePixels()
+
+    return [newChannelImage0, newChannelImage1]
   }
 }
