9d000b30628c41ead0bcc603c27cc6344b1e35f9
Connor de la Cruz <connor.c.delacruz@gmail.com>
2024-01-22 09:59:41 -0600

    code cleanup + extraction + render image to Graphics object

--- src/ChannelShiftSketch.jsx
+++ src/ChannelShiftSketch.jsx
@@ -5,61 +5,102 @@ export function ChannelShiftSketch(p5) {
   const B_OFFSET = 2
   const A_OFFSET = 3
 
-  let sourceImage, rImage, gImage, bImage
+  // Source image + RGB channel images
+  let sourceImage, sourceRImage, sourceGImage, sourceBImage
+  // Graphics object for preview + RGB channel images
+  let previewGraphics
 
+
+  // ================================================================================
+  // p5 Sketch Methods
+  // ================================================================================
+
+  /**
+   * Preload source image
+   */
   p5.preload = () => {
     sourceImage = p5.loadImage('test0.jpg')
   }
 
+
+  /**
+   * p5 setup
+   */
   p5.setup = () => {
     // Match window width, scale height accordingly
     p5.createCanvas(p5.windowWidth, (p5.windowWidth / sourceImage.width) * sourceImage.height)
-    // Due to weird scoping, need to define windowResized() here so it can use sourceImage dimensions
+
+    /**
+     * p5 windowResized
+     *
+     * Due to weird scoping, need to define windowResized() here so it can use sourceImage dimensions
+     */
     p5.windowResized = () => {
       p5.resizeCanvas(p5.windowWidth, (p5.windowWidth / sourceImage.width) * sourceImage.height)
     }
 
+    // Graphics object that will be drawn with the RGB layers on it
+    previewGraphics = p5.createGraphics(sourceImage.width, sourceImage.height)
+
     // Extract color channels
+    initializeRGBImages(sourceImage)
+  }
+
+
+  /**
+   * p5 draw
+   */
+  p5.draw = () => {
+    // TODO TESTING
+    let [newRImage, newBImage] = swapChannels(sourceRImage, R_OFFSET, sourceBImage, B_OFFSET)
+
+    // Blend RGB channels
+    previewGraphics.background(0)
+    previewGraphics.blend(newRImage, 0, 0, sourceRImage.width, sourceRImage.height, 0, 0, previewGraphics.width, previewGraphics.height, p5.ADD)
+    previewGraphics.blend(sourceGImage, 0, 0, sourceGImage.width, sourceGImage.height, 0, 0, previewGraphics.width, previewGraphics.height, p5.ADD)
+    previewGraphics.blend(newBImage, 0, 0, sourceBImage.width, sourceBImage.height, 0, 0, previewGraphics.width, previewGraphics.height, p5.ADD)
+
+    // Render to screen
+    p5.image(previewGraphics, 0, 0, p5.width, p5.height)
+  }
+
+
+  // ================================================================================
+  // Helper functions
+  // ================================================================================
+
+  /**
+   * Extract RGB channels from sourceImage and initialize RGB image variables
+   */
+  function initializeRGBImages() {
     // Initialize to blank images
-    rImage = p5.createImage(sourceImage.width, sourceImage.height)
-    rImage.loadPixels()
-    gImage = p5.createImage(sourceImage.width, sourceImage.height)
-    gImage.loadPixels()
-    bImage = p5.createImage(sourceImage.width, sourceImage.height)
-    bImage.loadPixels()
+    sourceRImage = p5.createImage(sourceImage.width, sourceImage.height)
+    sourceRImage.loadPixels()
+    sourceGImage = p5.createImage(sourceImage.width, sourceImage.height)
+    sourceGImage.loadPixels()
+    sourceBImage = p5.createImage(sourceImage.width, sourceImage.height)
+    sourceBImage.loadPixels()
     // Copy color channels from sourceImage.pixels
     // This is a 1D array that stores RGBA values. See docs for details:
     // https://p5js.org/reference/#/p5.Image/pixels
     sourceImage.loadPixels()
     for (let i = 0; i < sourceImage.pixels.length; i += 4) {
       // Red
-      rImage.pixels[i] = sourceImage.pixels[i]
+      sourceRImage.pixels[i] = sourceImage.pixels[i]
       // Green
-      gImage.pixels[i + 1] = sourceImage.pixels[i + 1]
+      sourceGImage.pixels[i + 1] = sourceImage.pixels[i + 1]
       // Blue
-      bImage.pixels[i + 2] = sourceImage.pixels[i + 2]
+      sourceBImage.pixels[i + 2] = sourceImage.pixels[i + 2]
       // Alpha
-      rImage.pixels[i + 3] = gImage.pixels[i + 3] = bImage.pixels[i + 3] = 255
+      sourceRImage.pixels[i + 3] = sourceGImage.pixels[i + 3] = sourceBImage.pixels[i + 3] = 255
     }
-    rImage.updatePixels()
-    gImage.updatePixels()
-    bImage.updatePixels()
+    sourceRImage.updatePixels()
+    sourceGImage.updatePixels()
+    sourceBImage.updatePixels()
   }
 
-  p5.draw = () => {
-    p5.background(0)
-    // TODO: figure out how to wrap image when it extends past the canvas
-    // TODO TESTING
-    let [newRImage, newBImage] = swapChannels(rImage, R_OFFSET, bImage, B_OFFSET)
-    // Blend RGB channels
-    p5.blend(newRImage, 0, 0, rImage.width, rImage.height, 0, 0, p5.width, p5.height, p5.ADD)
-    // p5.blend(rImage, 0, 0, rImage.width, rImage.height, 0, 0, p5.width, p5.height, p5.ADD)
-    p5.blend(gImage, 0, 0, gImage.width, gImage.height, 0, 0, p5.width, p5.height, p5.ADD)
-    // p5.blend(bImage, 0, 0, bImage.width, bImage.height, 0, 0, p5.width, p5.height, p5.ADD)
-    p5.blend(newBImage, 0, 0, bImage.width, bImage.height, 0, 0, p5.width, p5.height, p5.ADD)
-  }
 
-  // TODO: doc n implement
+  // TODO: doc
   function swapChannels(channelImage0, channelOffset0, channelImage1, channelOffset1) {
     let newChannelImage0 = p5.createImage(channelImage0.width, channelImage1.height)
     newChannelImage0.loadPixels()
