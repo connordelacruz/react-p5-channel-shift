ba7c0f56c1f83c2658895d1d2a36e9fe6279ce2b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2024-02-23 14:31:45 -0600

    fixed the fonts lol

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import {createRoot} from 'react-dom/client'
 import React from 'react'
 import App from './App'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './index.scss'
 
 const container = document.getElementById('root')
