3aab9d8f07767fde35e7131bc6ef9a405add48cd
Connor de la Cruz <connor.c.delacruz@gmail.com>
2024-09-12 10:37:07 -0500

    version bump

--- src/Constants.js
+++ src/Constants.js
@@ -4,7 +4,7 @@
 // App version
 export const VERSION_MAJOR = 1
 export const VERSION_MINOR = 1
-export const VERSION_PATCH = 0
+export const VERSION_PATCH = 1
 export const VERSION = `${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}`
 
 // ================================================================================
